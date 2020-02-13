cc8ebe94fcfb59b77c870409f63af2c559e53fb6
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-02-13 16:58:26 -0600

    Update 02_decisions_tests.cpp

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -7,19 +7,3 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Testing Letter Grade")
-{
-    REQUIRE(letter_grade("A") == 4);
-    REQUIRE(letter_grade("B") == 3);
-    REQUIRE(letter_grade("C") == 2);
-    REQUIRE(letter_grade("D") == 1);
-    REQUIRE(letter_grade("F") == 0);
-    REQUIRE(letter_grade(Invalid Option) == -1);
-}
-
-TEST_CASE("Calculate GPA")
-{
-    REQUIRE(calculate_gpa(12, 45)==3.75);
-    REQUIRE(calculate_gpa(120, 390)==3.25);
-    REQUIRE(calculate_gpa(90, 180)==2.00);
-}
