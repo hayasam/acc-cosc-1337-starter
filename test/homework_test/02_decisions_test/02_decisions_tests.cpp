b7a5f33e6196a1c3a59cf76d9afbf44573813271
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-02-13 16:52:55 -0600

    Update 02_decisions_tests.cpp

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,25 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
+#include "decisions.cpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Testing Letter Grade")
+{
+    REQUIRE(letter_grade("A") == 4);
+    REQUIRE(letter_grade("B") == 3);
+    REQUIRE(letter_grade("C") == 2);
+    REQUIRE(letter_grade("D") == 1);
+    REQUIRE(letter_grade("F") == 0);
+    REQUIRE(letter_grade(Invalid Option) == -1);
+}
+
+TEST_CASE("Calculate GPA")
+{
+    REQUIRE(calculate_gpa(12, 45)==3.75);
+    REQUIRE(calculate_gpa(120, 390)==3.25);
+    REQUIRE(calculate_gpa(90, 180)==2.00);
+}
